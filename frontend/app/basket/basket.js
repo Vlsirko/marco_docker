38df5a325611be81766f75f43358435104c63b7d
Sirenko Vlad <vlad.sirenko@corp.kismia.com>
2016-05-19 11:20:51 +0300

    basket processibg

--- frontend/app/basket/basket.js
+++ frontend/app/basket/basket.js
@@ -11,12 +11,15 @@ angular.module('MirrorStore.basket', ['ngRoute'])
 
     .controller('basketCtrl', function($scope, $routeParams, Product, $cookies) {
         $scope.basket = $cookies.getObject('basket');
+        $scope.empty = Object.keys($scope.basket).length === 0;
+
         Product.get({ids:  Object.keys($scope.basket).join(',')}, function (data) {
             $scope.products = data.results;
         });
 
         $scope.removeFromCard = function(id){
             delete $scope.basket[id];
+            $scope.empty = Object.keys($scope.basket).length === 0;
             $cookies.putObject('basket', $scope.basket)
         }
 
