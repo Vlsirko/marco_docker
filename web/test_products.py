da2e981227c1aa99d234119bb3494cc803b1fada
Vlsirko <Vlsirko@i.ua>
2016-06-22 15:15:56 +0300

    sdfsfs
    sdfsdf

--- web/test_products.py
+++ web/test_products.py
@@ -1,10 +1,12 @@
-from api.models.product import Product
-from api.models.seo_block import SeoBlock
-from api.models.category import Category
+from mirrorstore.models.product import Product
+from mirrorstore.models.seo_block import SeoBlock
+from mirrorstore.models.category import Category
+from mirrorstore.models.image import Image
 import random
 
 
 categories = Category.objects.get(url='1_2'), Category.objects.get(url='3-4')
+images = Image.objects.all()
 seo_block = SeoBlock.objects.get(pk=1)
 
 for i in range(0, 500):
@@ -13,6 +15,7 @@ for i in range(0, 500):
     product.url = 'test_product_title {}'.format(i)
     product.price = 100 + i
     product.category = categories[random.randrange(2)]
+    product.title_image = random.choice(images)
     product.is_sale = random.randrange(2) == 1
     product.is_preorder = random.randrange(2) == 1
     product.is_new = random.randrange(2) == 1
